71b1755b98eb19dfce0b83ecdae0b12418e33aa5
Philippe Clavier <pclavier92@gmail.com>
2020-05-16 11:41:36 -0300

    Fix marquee bug

--- src/client/common-components/song-card/song-card.js
+++ src/client/common-components/song-card/song-card.js
@@ -39,13 +39,13 @@ const SongCard = ({ song, duration, progress }) => {
           </div>
           <div className="content">
             <div className="song-details">
-              <RotatingText maxHeight={MAX_H4_HEIGHT}>
+              <RotatingText key={name} maxHeight={MAX_H4_HEIGHT}>
                 <h4>{name}</h4>
               </RotatingText>
-              <RotatingText maxHeight={MAX_H5_HEIGHT}>
+              <RotatingText key={artits} maxHeight={MAX_H5_HEIGHT}>
                 <h5>{artits}</h5>
               </RotatingText>
-              <RotatingText maxHeight={MAX_H6_HEIGHT}>
+              <RotatingText key={album} maxHeight={MAX_H6_HEIGHT}>
                 <h6>{album}</h6>
               </RotatingText>
 
